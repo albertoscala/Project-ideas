79af4287d88076c400108f462580914c9a625179
Alberto Scala <31989626+albertoscala@users.noreply.github.com>
2022-03-22 18:01:10 +0100

    Correction

--- Pi nth digit/pi_nth_digit.py
+++ Pi nth digit/pi_nth_digit.py
@@ -1,14 +1,17 @@
 from math import factorial
 from decimal import Decimal, getcontext
 
-getcontext().prec = 1000
+
 
 def pi_nth_alg(n):
+    # Setting the floating accuracy to N
+    getcontext().prec = n+1
+
     # Declaring PI as a decimal for a better precision
     pi = Decimal()
 
     # Using the formula for N+2 times to obtain PI
-    for i in range(n+2):
+    for i in range(n):
         div = Decimal(((-1) ** i) * (factorial(6 * i)) * (13591409 + 545140134 * i))
         den = Decimal((factorial(3 * i)) * (factorial(i) ** 3) * (640320 ** (3 * i)))
         pi += div / den
@@ -19,11 +22,8 @@ def pi_nth_alg(n):
     # Reversing the PI
     pi = 1 / pi
 
-    # Converting the PI to string to get the N digit
-    pi = str(pi)
-
-    # Returning PI's N digit
-    return pi[n]
+    # Returning PI's to Nth digit
+    return pi
 
 if __name__ == '__main__':
     n = int(input('Insert n position of the digit: '))
